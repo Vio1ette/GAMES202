9edb5a77725320ec31cc87675f70612795b4d808
Violette <1372597331@qq.com>
2022-04-05 08:16:53 +0800

    preliminary finish

--- hw1/homework1/src/shaders/phongShader/phongFragment.glsl
+++ hw1/homework1/src/shaders/phongShader/phongFragment.glsl
@@ -30,11 +30,12 @@ uniform sampler2D uShadowMap;
 
 varying vec4 vPositionFromLight;
 
-highp float rand_1to1(highp float x ) { 
+highp float rand_1to1(highp float x ) {
   // -1 -1
   return fract(sin(x)*10000.0);
 }
 
+// 返回一个 [0,1] 之间的随机值
 highp float rand_2to1(vec2 uv ) { 
   // 0 - 1
 	const highp float a = 12.9898, b = 78.233, c = 43758.5453;
@@ -51,6 +52,8 @@ float unpack(vec4 rgbaDepth) {
 
 vec2 poissonDisk[NUM_SAMPLES];
 
+// 泊松采样
+// 在 randomSeed周围进行采样？
 void poissonDiskSamples( const in vec2 randomSeed ) {
 
   float ANGLE_STEP = PI2 * float( NUM_RINGS ) / float( NUM_SAMPLES );
@@ -87,29 +90,77 @@ void uniformDiskSamples( const in vec2 randomSeed ) {
   }
 }
 
+
+// 返回平均d_Blocker
+// 注意zReceiver的作用，计算所有遮挡物的平均dBlocker，zReceiver用来判断是不是遮挡物
 float findBlocker( sampler2D shadowMap,  vec2 uv, float zReceiver ) {
-	return 1.0;
+  poissonDiskSamples(uv);
+
+  //首先，确定一个范围，取这个范围内得blocker的平均depth
+
+  //@@
+  float dBlockerRange = 1.0 / 400.0 * 50.0;
+
+  float dBlocker = 0.0;
+  int count = 0;
+  for(int i=0;i<BLOCKER_SEARCH_NUM_SAMPLES;i++){
+
+      vec4 map_value = texture2D(shadowMap, uv + poissonDisk[i] * dBlockerRange);
+      float map_depth =unpack(map_value);
+
+      if(map_depth + 0.01> zReceiver)continue;
+
+      dBlocker+=map_depth;
+      count+=1;
+  }
+
+  //@@
+  if(count == BLOCKER_SEARCH_NUM_SAMPLES)return 2.0;
+
+	return dBlocker/float(count);
 }
 
-float PCF(sampler2D shadowMap, vec4 coords) {
+float PCF(sampler2D shadowMap, vec4 coords, float filterSize) {
   // 与 shadowMap 不同，PCF不再是简单的二元值（1，0），而是根据 filter size 取一个中间值
-  // 怎么取
-
-
-
-  return 1.0;
+  // 怎么取，首先 coords的 x,y是属于[0,1]的
+  // 答案，已经内置泊松采样，直接用就行了
+  poissonDiskSamples(coords.xy);
+  // 我不知道这个泊松采样是怎么运作的，当然也就不知道它有个采样范围
+  // 不知道这个filterSize是怎么得出来?
+  // 如果 filterSize过大，则会导致在很大的范围内进行平均，就会是阴影变得过于模糊
+  int ret=0;
+  float camera_depth = coords.z;
+  for(int i=0;i<NUM_SAMPLES;i++){
+    vec4 map_value = texture2D(shadowMap,coords.xy + poissonDisk[i] * filterSize);
+    float map_depth = unpack(map_value);  
+    // 1e-2是bias
+    if(abs(map_depth-camera_depth)<1e-2||map_depth>camera_depth)ret+=1;
+    // 这个 0.01 是 bias
+    // if(map_depth > camera_depth - 0.01) ret+=1;
+  }
+  // float()强制转换
+  return float(ret) / float(NUM_SAMPLES);
 }
 
 float PCSS(sampler2D shadowMap, vec4 coords){
 
   // STEP 1: avgblocker depth
+  
+  float avgBlocker_depth = findBlocker(shadowMap,coords.xy,coords.z); 
 
   // STEP 2: penumbra size
 
-  // STEP 3: filtering
-  
-  return 1.0;
+  //@@
+  if(avgBlocker_depth<EPS)return 1.0;
+  if(avgBlocker_depth>2.0)return 0.0;
 
+  //感觉就是调参了，为了让效果更好
+  float w_light = 1.0/50.0;
+
+  float w_penumbra = (coords.z - avgBlocker_depth) * w_light / avgBlocker_depth;
+
+
+  return PCF(shadowMap,coords, w_penumbra);
 }
 
 
@@ -173,9 +224,9 @@ void main(void) {
     shadowCoord= shadowCoord*0.5+0.5;
 
   float visibility;
-  visibility = useShadowMap(uShadowMap, vec4(shadowCoord, 1.0));
-  //visibility = PCF(uShadowMap, vec4(shadowCoord, 1.0));
-  //visibility = PCSS(uShadowMap, vec4(shadowCoord, 1.0));
+  // visibility = useShadowMap(uShadowMap, vec4(shadowCoord, 1.0));
+  // visibility = PCF(uShadowMap, vec4(shadowCoord, 1.0), 1.0/400.0);
+  visibility = PCSS(uShadowMap, vec4(shadowCoord, 1.0));
 
   vec3 phongColor = blinnPhong();
 
